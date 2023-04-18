30264870bbe8b52051a166f68bf839f3af263bc2
wael senani <111441399+wael-boss@users.noreply.github.com>
2023-04-18 06:42:39 +0100

    animation system for merge sort done

--- src/pages/SortingVisualiser.js
+++ src/pages/SortingVisualiser.js
@@ -1,134 +1,67 @@
 import { useEffect, useMemo, useRef } from 'react'
 import { useState } from 'react'
 import '../css/SortingVisualiser.css'
+import { type } from '@testing-library/user-event/dist/type'
 const SortingVisualiser = () => {
-    const animations=[]
     const [bars ,setBars]=useState([])
     // sorting algos
     // merge sort
-    const merge=(firstHalf ,secondHalf)=>{
-        const result=[]
-        while(firstHalf.length && secondHalf.length){
-            if(firstHalf[0]<secondHalf[0]){
-                result.push(firstHalf.shift()) 
-            }else{
-                result.push(secondHalf.shift())
-            }
-        }
-        return [...result ,...firstHalf ,...secondHalf]
-    }
-    const mergeSort=(array)=>{
-        if(array.length<2) return array
-        const midPoint=Math.floor(array.length/2)
-        const firstHalf=array.splice(0 ,midPoint)
-        return merge(mergeSort(firstHalf) ,mergeSort(array))
-    }
-    // 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    const testMerge=(array ,firstParams ,secondParams)=>{
+    const merge=(array ,animations ,firstParams ,secondParams)=>{
+        // indicators to check the left and right sides of the array comparing them in the procces
         let leftIndicator=0
         let rightIndicator=firstParams.e-firstParams.s
+        // the point in the array where correct position values would go
         let arrayPosition=firstParams.s
+        // a temporary array to hold the current section and avoid duplicate values in the final array
         let tempArr=array.slice(firstParams.s ,secondParams.e)
+        // a loop that runs until one of the indicators reached the end of its array
         while(leftIndicator+firstParams.s<firstParams.e && rightIndicator+firstParams.s<secondParams.e){
-            console.log(leftIndicator ,rightIndicator)
-            console.log(`comparing ${tempArr[leftIndicator]} and ${tempArr[rightIndicator]}`)
+            // add a frame of comparison to the animations
+            animations.push({type:'compare',A:leftIndicator+firstParams.s,B:rightIndicator+firstParams.s})
+            // look for the smaller value between the left and right side and sort it 
             if(tempArr[leftIndicator]<tempArr[rightIndicator]){
-                console.log(`was ${array[arrayPosition]} became ${tempArr[leftIndicator]}`)
+                // save a frame of change to the animation
+                animations.push({type:'change',index:arrayPosition,value:tempArr[leftIndicator]})
                 array[arrayPosition]=tempArr[leftIndicator]
                 leftIndicator++
             }else{
-                console.log(`was ${array[arrayPosition]} became ${tempArr[rightIndicator]}`)
+                // save a frame of change to the animation
+                animations.push({type:'change',index:arrayPosition,value:tempArr[rightIndicator]})
                 array[arrayPosition]=tempArr[rightIndicator]
                 rightIndicator++
             }
+            // increment the position for the next value
             arrayPosition++
         }
+        // pick up leftover values and add them to the already sorted array
         while(leftIndicator+firstParams.s<firstParams.e){
+            animations.push({type:'change',index:arrayPosition,value:tempArr[leftIndicator]})
             array[arrayPosition]=tempArr[leftIndicator]
             leftIndicator++
             arrayPosition++
         }
         while(rightIndicator+firstParams.s<secondParams.e){
+            animations.push({type:'change',index:arrayPosition,value:tempArr[rightIndicator]})
             array[arrayPosition]=tempArr[rightIndicator]
             rightIndicator++
             arrayPosition++
         }
-        console.log(array)
-        return {s:firstParams.s,e:secondParams.e}
+        // look for the actual end of the function to return the animations array
+        if(tempArr.length===array.length){
+            return animations
+        }else{
+            return {s:firstParams.s,e:secondParams.e}
+        }
 }
-    const testMergeSort=(array ,s=0 ,e=array.length)=>{
+    const mergeSort=(array, animations=[] ,s=0 ,e=array.length)=>{
         const currentArray=array.slice(s ,e)
-        // console.log(currentArray)
+        // break point to avoid infinite recursion
         if(currentArray.length<2) return {s:s,e:e}
+        // middle index of the array
         const midPoint=Math.floor(currentArray.length/2)
-        return testMerge(array ,testMergeSort(array ,s ,s+midPoint),testMergeSort(array ,s+midPoint ,e))
+        // create a recursion to cut down the array as small as possible then re-assemble it using the merge function
+        return merge(array ,animations ,mergeSort(array ,animations ,s ,s+midPoint),mergeSort(array ,animations ,s+midPoint ,e)) 
     }
-
-
-
-
-
-
-
-
-
-
-    const animationFunc=()=>{
-        let factor=100
-        let i=100
-        animations.map(frame=>{
-            if(frame.type==='compare'){
-                setTimeout(()=>{
-                    bars[frame.A].classList.add('compared')
-                    bars[frame.B].classList.add('compared')
-                },i)
-                setTimeout(()=>{
-                    bars[frame.A].classList.remove('compared')
-                    bars[frame.B].classList.remove('compared')
-                },i+factor)
-            }else if(frame.type==='swap'){
-                let result=bars
-                const tempStorage=result[frame.A]
-                result[frame.A]=result[frame.B]
-                result[frame.B]=tempStorage
-                setTimeout(()=>{
-                    setBars(result)
-                },i)
-            }
-            i+=factor
-        })
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
     // bubble sort
     const bubbleSort=(array)=>{
     if(array.length<2) return array
@@ -168,14 +101,47 @@ const SortingVisualiser = () => {
     const [mainArray ,setMainArray]=useState([])
     const [max ,setMax]=useState(100)
     const [algo ,setAlgo]=useState(1)
+    const [isAnimating ,setIsAnimating]=useState(false)
     const graphRef=useRef()
     const maxNumber=400 //400 for best proformence
     const minNumber=4   //4 for best proformence
-    const animator=()=>{
-        // setInterval(()=>{
-            //     setMainArray(frame)
-        // },100)
-        // )
+    const animationFunc=(arr)=>{
+        // the number of mileseconds in between every frame
+        let factor=width<10 ? 500 : width<20 ? 100 : width<50 ? 50 : 10
+        // history of the last time out to create a consitent flow 
+        let i=factor
+        // get animatins from the chosen algorithm
+        const animations=algo===1 ? mergeSort(arr) : algo===2 ? quickSort(arr) : algo===3 ? {/*heap */}: bubbleSort(arr)
+        setTimeout(()=>{
+            setIsAnimating(false)
+        },animations.length*factor)
+        // loop through the frames and set timeouts accodingly
+        animations.map(frame=>{
+            // a frame comparing two values and changing their color to red
+            if(frame.type==='compare'){
+                setTimeout(()=>{
+                    bars[frame.A].style.backgroundColor='red'
+                    bars[frame.B].style.backgroundColor='red'
+                    setTimeout(()=>{
+                        bars[frame.A].style.backgroundColor=''
+                        bars[frame.B].style.backgroundColor=''
+                    },factor)
+                },i)
+            // a frame swaping two values
+            }else if(frame.type==='swap'){
+            // a frame changing the height of a value
+            }else if(frame.type==='change'){
+                setTimeout(()=>{
+                    bars[frame.index].title=frame.value
+                    bars[frame.index].style.height=`${frame.value}px`
+                    bars[frame.index].style.backgroundColor=`green`
+                    setTimeout(()=>{
+                        bars[frame.index].style.backgroundColor=`var(--colorScale3)`
+                    },factor)
+                },i)
+            }
+            i+=factor
+        })
     }
     const newArray=()=>{
         const arr=[]
@@ -200,17 +166,26 @@ const SortingVisualiser = () => {
     <main>
         <section id="sortingOptionsBar">
             <div className="sortingOption" id="generateArrayOption">
-                <button onClick={()=>setMainArray(newArray())}>Generate New Array</button>
+                <button
+                style={{backgroundColor:isAnimating ? 'var(--colorScale5)' : ''}}
+                onClick={()=>{
+                    if(isAnimating) return
+                    setMainArray(newArray())
+                    }}>Generate New Array</button>
             </div>
             <div className="sortingOption" id="silderOption">
                 <p>alter array size and sorting speed</p>
                 <input 
+                style={{background:isAnimating ? 'var(--colorScale5)' : ''}}
                 id='widthRange'
                 type='range'
                 min={minNumber}
                 max={max}
                 value={width}
-                onChange={e=>setWidth(Number(e.target.value))}
+                onChange={e=>{
+                    if(isAnimating) return
+                    setWidth(Number(e.target.value))
+                }}
                 />
             </div>
             <div className="sortingOption" id="alogorithmOption">
@@ -220,16 +195,13 @@ const SortingVisualiser = () => {
                 <p style={{borderBottomColor:algo===4? 'var(--colorScale2)' : ''}} onClick={()=>setAlgo(4)}>Bubble</p>
             </div>
             <div className="sortingOption" id="sortOption">
-                <button onClick={()=>{
-                    setAlgoTimer({start:Date.now() ,end:null})
-                    setMainArray(
-                    algo===1 ? mergeSort(mainArray) : 
-                    algo===2 ? quickSort(mainArray):
-                    algo===3 ? {/*heap */}:
-                    bubbleSort(mainArray)
-                )
-                }}>sort !</button>
-                <button onClick={()=>{testMergeSort(mainArray)}}>test merge</button>
+                <button
+                style={{backgroundColor:isAnimating ? 'var(--colorScale5)' : ''}}
+                onClick={()=>{
+                    if(isAnimating) return
+                    // setIsAnimating(true) sorts it before the animation
+                    animationFunc(mainArray)
+                    }}>sort !</button>
             </div>
         </section>
         <section id="sortingSection" ref={graphRef}>
